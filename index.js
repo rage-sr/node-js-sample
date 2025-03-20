1e9478e51d13937bc60e0b4cf76ee55c0b3ffe61
rage-sr <ragecrazy2829@gmail.com>
2025-03-20 12:03:34 +0530

    Update index.js
    Signed-off-by: rage-sr <ragecrazy2829@gmail.com>

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello alll am santhu!')
+  response.send('Hello alll!')
 })
 
 app.listen(app.get('port'), function() {
